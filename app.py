2ac85546897037ed6b9c149a71002c40ea5ca2bb
ADPriceless <47830638+ADPriceless@users.noreply.github.com>
2024-04-15 12:29:47 +0100

    replace original app with new functionality (Issue #21)

--- app.py
+++ app.py
@@ -1,357 +1,168 @@
 """Segmentation app:
-Allows user to upload an image and a mask, then makes a prediction,
-compares it to the mask and calculates the IoU"""
+Displays a single image from a test dataset with options to:
+- retrieve new images;
+- overlay the mask, predicion and IoU onto it;
+- and change the threshold for the prediction.
+"""
 
-from dataclasses import dataclass
-from typing import Optional
+
+import random
 
 from PIL import Image
 import streamlit as st
 import torch
+from torch import Tensor
 from torchvision.transforms.functional import to_tensor, to_pil_image, resize
-import torchvision.transforms.v2 as tv_transforms
 from torchvision.utils import draw_segmentation_masks
 
-from models.fcn_factory import FcnFactory
-from preprocess.transforms import rgba_to_rgb
+import app_state
 
 
-@dataclass
-class ImageModel:
-    """Base class to store data for an image."""
-    data: Optional[Image.Image]
-    caption: str
-    use_col_width: bool
-
-    def __init__(
-        self, caption: str, data: Image.Image = None, use_col_width=True
-    ) -> None:
-        self.caption = caption
-        self.data = data
-        self.use_col_width = use_col_width
-
-    @property
-    def mode(self):
-        """Return the mode of the image e.g. RGB, L, etc."""
-        if self.data:
-            return self.data.mode
-        return None
-
-
-@dataclass
-class UploadImageModel(ImageModel):
-    """A class to store data for the images that the user uploads."""
-    upload_msg: str
-    supported_modes: list[str]
-    supported_types: Optional[list[str]]
-
-    def __init__(
-        self, caption: str, upload_msg: str, supported_modes: list[str],
-        supported_types: tuple[str] = None, data: Image.Image = None, use_col_width=True
-    ) -> None:
-        super().__init__(caption, data, use_col_width)
-        self.upload_msg = upload_msg
-        self.supported_modes = supported_modes
-        if supported_types is None:
-            self.supported_types = ('png', 'jpg', 'jpeg')
-        else:
-            self.supported_types = supported_types
-
-    @property
-    def mode_supported(self) -> bool:
-        """Returns true if the """
-        if self.data is None:
-            return False
-        return self.data.mode in self.supported_modes
-
-    def convert_to(self, mode: str):
-        """Convert the image to specified mode."""
-        if self.data is not None:
-            self.data = self.data.convert(mode)
+def load_sample_image() -> Image.Image:
+    """Open the chosen image and apply whichever overlay is selected."""
+    _load_img_mask_paths()
+    input_img = _open_image(astype='tensor')
+    opt = st.session_state.overlay
+    if opt == 'True Mask':
+        return _overlay_mask_on_img(
+            input_img, _open_mask(), overlay_colours=['yellow']
+        )
+    if opt == 'Prediction':
+        prediction = _predict(input_img)
+        return _overlay_mask_on_img(
+            input_img, prediction, overlay_colours=['yellow']
+        )
+    if opt == 'IoU':
+        intersection, union = _calc_intersection_and_union(input_img)
+        _iou_score(intersection, union)
+        iou_mask = _iou_mask(intersection, union)
+        return _overlay_mask_on_img(
+            input_img, iou_mask, overlay_colours=['yellow', 'purple']
+        )
+    return _open_image(astype='image')
 
 
-@dataclass
-class OverlayImageModel(ImageModel):
-    """A class to store data for images where a mask is overlaid."""
-    alpha: float
-    colours: list[str]
-
-    def __init__(
-        self, caption: str, data: Image.Image = None, use_col_width=True,
-        alpha=0.5, colours='yellow'
-    ) -> None:
-        super().__init__(caption, data, use_col_width)
-        self.alpha = alpha
-        self.colours = colours
+def _load_img_mask_paths():
+    files = tuple(st.session_state.test_ds_path.glob('*.png'))
+    st.session_state.img_path, st.session_state.mask_path = \
+        files[st.session_state.sample_idx:st.session_state.sample_idx+2]
 
 
-@dataclass(init=True)
-class AppModel:
-    """Stores the data for the application."""
-    # General
-    title: str
-    # Image
-    upload_header: str
-    input_image: UploadImageModel
-    mask_image: UploadImageModel
-    # Classifier
-    solar_clf_path: str
-    input_trans: list
-    # Overlaid Images
-    overlay_header: str
-    pred_overlay: OverlayImageModel
-    mask_overlay: OverlayImageModel
-    # Scoring
-    scoring_header: str
-    iou_overlay: OverlayImageModel
+def _open_image(astype: str = 'image') -> Tensor | Image.Image:
+    img = Image.open(st.session_state.img_path)
+    if astype == 'image':
+        return img
+    if astype == 'tensor':
+        # `to_tensor` seems to convert to float, so convert back to uint8
+        return (to_tensor(img) * 255).to(torch.uint8)
+    raise ValueError(f'Unknown value for `astype`: {astype}')
 
 
-def make_default_app_model() -> AppModel:
-    """Initialises the AppModel. Edit values to alter app properties."""
-    return AppModel(
-        # General
-        title='Solar Panel Segmentation',
-        # Image
-        upload_header='Upload an Image and/or Mask',
-        input_image = UploadImageModel(
-            caption='Uploaded image.',
-            upload_msg='Upload an image to input to the classifier.',
-            supported_modes=['RGB', 'RGBA', 'P']
-        ),
-        mask_image=UploadImageModel(
-            caption='Uploaded mask.',
-            upload_msg='Upload a mask.',
-            supported_modes=['L']
-        ),
-        # Classifier
-        solar_clf_path='saved_models/model_20240328_074804_4',
-        input_trans=[tv_transforms.Lambda(rgba_to_rgb)],
-        # Overlaid Images
-        overlay_header='Prediction vs. True Mask',
-        pred_overlay=OverlayImageModel('Prediction'),
-        mask_overlay=OverlayImageModel('True Mask'),
-        # Scoring
-        scoring_header='IoU Score: ',
-        iou_overlay=OverlayImageModel(
-            caption='IoU: Yelow = Intersection, Purple = Union',
-            colours=['yellow', 'purple']
-        ),
+def _overlay_mask_on_img(
+    img: Tensor, mask: Tensor, overlay_colours: list[str]
+) -> Image.Image:
+    overlaid = draw_segmentation_masks(
+        img, mask, alpha=st.session_state.overlay_alpha, colors=overlay_colours
     )
+    return to_pil_image(overlaid)
 
 
-class View:
-    """A class to handle displaying items on the page."""
-    def __init__(self) -> None:
-        # __init__ defines page layout
-        pass
-
-    def show_title(self, title: str) -> None:
-        st.title(title)
-
-    def setup_columns(self):
-        return st.columns(2)
-
-    def show_header(self, header: str) -> None:
-        st.header(header)
-
-    def get_input_image(self, message: str, supported_types: list[str]):
-        return st.file_uploader(message, type=supported_types)
-
-    def show_image(self, img: ImageModel):
-        st.image(
-            img.data,
-            caption=img.caption,
-            use_column_width=img.use_col_width
-        )
-
-    def show_mode_not_supported_warning(self, mode: str):
-        st.warning(f'Image mode "{mode}" not supported as model input')
-
-    def warning(self, msg):
-        st.warning(msg)
+def _open_mask() -> Tensor:
+    return to_tensor(Image.open(st.session_state.mask_path)) > 0.
 
 
-class UploadController:
-    """Handles logic related to uploading and displaying images."""
-    def __init__(self, view: View, model: AppModel) -> None:
-        self.model = model
-        self.view = view
-
-    def upload_show_input_img(self):
-        """Upload and show the input image."""
-        self._upload_photo_to(self.model.input_image)
-        if not self.model.input_image.mode_supported:
-            self.view.show_mode_not_supported_warning(self.model.input_image.mode)
-            return
-        # Always convert to RGB since colour mode 'P' is
-        # ok for displaying but will not be accepted by
-        # the classifier
-        self.model.input_image.convert_to('RGB')
-        self.view.show_image(self.model.input_image)
-
-    def _upload_photo_to(self, input_img: UploadImageModel) -> None:
-        uploaded_file = self.view.get_input_image(input_img.upload_msg, input_img.supported_types)
-        if uploaded_file is not None:
-            input_img.data = Image.open(uploaded_file)
-        else:
-            input_img.data = None
-
-    def upload_show_mask_img(self):
-        """Upload and show the mask."""
-        self._upload_photo_to(self.model.mask_image)
-        if not self.model.mask_image.mode_supported:
-            self.view.show_mode_not_supported_warning(self.model.mask_image.mode)
-            return
-        self.view.show_image(self.model.mask_image)
+def _predict(input_img: Tensor) -> Tensor:
+    if not st.session_state.prediction_valid:
+        # Prevent the app from recalculating the prediction over and
+        # again (Streamlit reruns entire script for any UI change).
+        # `prediction_valid` is reset whenever a new image is grabbed.
+        st.session_state.output = _clf_output(input_img)
+        st.session_state.prediction_valid = True
+    pred = (torch.softmax(st.session_state.output, dim=1) * 100.) > st.session_state.clf_threshold
+    return resize(pred, input_img.shape[-2:])[0, 1] # return [h x w] of prediction
 
 
-class ClfController:
-    """A class to coordinate interactions between the classifier and 
-    view."""
-    def __init__(self, view: View, model: AppModel) -> None:
-        self.view = view
-        self.model = model
-        self.factory = None
-        self.classifier = None
-        self.prediction = None
-        self._load_solar_clf()
-        self._load_input_transforms()
-
-    def _load_solar_clf(self) -> None:
-        self.factory = FcnFactory(n_classes=2)
-        loaded_clf = self.factory.make_fcn('resnet50')
-        loaded_clf.load_state_dict(torch.load(
-            self.model.solar_clf_path, map_location=torch.device('cpu')
-        ))
-        loaded_clf.eval()
-        self.classifier = loaded_clf
-
-    def _load_input_transforms(self) -> None:
-        self.model.input_trans.append(self.factory.input_transforms)
-        self.model.input_trans = tv_transforms.Compose(self.model.input_trans)
-
-    def show_prediction(self):
-        """Displays the classifier's prediction overlaid onto the input image."""
-        self._predict()
-        self._make_segmentation(self.model.pred_overlay, self._format_prediction())
-        self.view.show_image(self.model.pred_overlay)
-
-    def _predict(self) -> torch.Tensor:
-        """Predict the segmentation mask of an image."""
-        processed_img = self.model.input_trans(to_tensor(self.model.input_image.data))
-        # Expand to shape [batch_size, n_classes, h, w] required for model
-        processed_img = processed_img.expand([1, *processed_img.shape])
-        # The output contains two classes: background (class 0) and solar panels
-        # (class 1). Extract solar panels the class.
-        output = self.classifier(processed_img)['out']
-        self.prediction = torch.softmax(output, dim=1) > 0.5
-
-    def _make_segmentation(self, img: OverlayImageModel, mask: torch.Tensor) -> None:
-        """Display the segmentation mask (predicted or true) of an image"""
-        # Overlay mask onto image
-        processed_img = (to_tensor(self.model.input_image.data) * 255).type(torch.uint8)
-        segmented = draw_segmentation_masks(
-            processed_img, mask, alpha=img.alpha, colors=img.colours
-        )
-        img.data = to_pil_image(segmented)
-
-    def _format_prediction(self) -> None:
-        """Format the prediction to be of shape [h, w] and chosen size"""
-        # PIL is WxH, where as torch is HxW
-        to_size = self.model.input_image.data.size[1], self.model.input_image.data.size[0]
-        return resize(self.prediction, to_size)[0, 1]
-
-    def show_mask(self):
-        """Show the mask overlaid on the input image."""
-        self._make_segmentation(self.model.mask_overlay, self._format_true_mask())
-        self.view.show_image(self.model.mask_overlay)
-
-    def _format_true_mask(self) -> torch.Tensor:
-        return to_tensor(self.model.mask_image.data) > 0.
-
-    def calc_iou(self) -> float:
-        """Calculate the IoU for a single sample"""
-        pred_mask = self._format_prediction()
-        true_mask = self._format_true_mask()
-        intersection = true_mask.logical_and(pred_mask).sum()
-        union = true_mask.logical_or(pred_mask).sum()
-        # Check if union is zeros.
-        if union == 0.:
-            # This means that the prediction and output
-            # are both all zeros.
-            return 1.
-        return intersection / union
-
-    def show_iou(self):
-        """Show the IoU overlaid on the input image."""
-        pred_mask  = self._format_prediction()
-        true_mask = self._format_true_mask()
-        intersection = true_mask.logical_and(pred_mask)
-        union = true_mask.logical_or(pred_mask)
-        # Remove union mask where intersection is
-        union = union.logical_xor(intersection)
-        masks = torch.concat((intersection, union), dim=0)
-        self._make_segmentation(self.model.iou_overlay, masks)
-        self.view.show_image(self.model.iou_overlay)
+def _clf_output(input_img: Tensor) -> Tensor:
+    processed_img = st.session_state.in_transforms(input_img)
+    # Expand to shape [batch_size, n_classes, h, w] required for model
+    processed_img = processed_img.expand([1, *processed_img.shape])
+    # The output contains two classes: background (class 0) and solar panels
+    # (class 1). Extract solar panels the class.
+    return st.session_state.classifier(processed_img)['out']
 
 
-class MainController:
-    """Class to coordinate interactions between the other controllers,
-    the model and the view."""
-    def __init__(self, view: View, model: AppModel) -> None:
-        self.view = view
-        self.model = model
-        self.upload_controller = UploadController(view, model)
-        self.clf_controller = ClfController(view, model)
+def _calc_intersection_and_union(input_img: Tensor) -> tuple[Tensor, Tensor]:
+    pred = _predict(input_img) # make a prediction if haven't already
+    true = _open_mask()
+    intersection = true.logical_and(pred)
+    union = true.logical_or(pred)
+    return intersection, union
 
-    def run(self):
-        """The overarching logic to run the application."""
-        self.view.show_title(self.model.title)
-        self.view.show_header(self.model.upload_header)
-        left, right = self.view.setup_columns()
-        with left:
-            self.upload_controller.upload_show_input_img()
-        with right:
-            self.upload_controller.upload_show_mask_img()
-        self.view.show_header(self.model.overlay_header)
-        left, right = self.view.setup_columns()
-        if not self._img_valid():
-            return
-        with left:
-            self.clf_controller.show_prediction()
-        if not (self._img_mask_valid() and self._img_mask_same_size()):
-            return
-        with right:
-            self.clf_controller.show_mask()
-        iou = self.clf_controller.calc_iou()
-        self.view.show_header(self.model.scoring_header + f'{iou:.2f}')
-        self.clf_controller.show_iou()
 
-    def _img_valid(self) -> bool:
-        return self.model.input_image.mode_supported
+def _iou_mask(intersection: Tensor, union: Tensor) -> Tensor:
+    union = union.logical_xor(intersection) # Remove union mask where intersection is
+    return torch.concat((intersection, union), dim=0)
 
-    def _img_mask_valid(self) -> bool:
-        if self._img_valid() and self._mask_valid():
-            return True
-        self.view.warning('Image or mask not valid')
-        return False
 
-    def _mask_valid(self) -> bool:
-        return self.model.mask_image.mode_supported
+def _iou_score(intersection: Tensor, union: Tensor) -> None:
+    union_sum = union.sum().item()
+    # If union is zero, neither pred or mask have solar panels. So,
+    # model has made perfect prediction. Also prevent zero-division.
+    if union_sum == 0.:
+        st.session_state.iou = 1.
+    st.session_state.iou = intersection.sum().item() / union_sum
 
-    def _img_mask_same_size(self) -> bool:
-        if self.model.input_image.data.size == self.model.mask_image.data.size:
-            return True
-        self.view.warning('Image and mask must have same width and height')
-        return False
+
+def choose_random_img_mask_pair():
+    """Select a random sample from the dataset."""
+    # One sample is an image/mask pair
+    choice = random.randint(0, 2*st.session_state.num_samples)
+    # Make even to point to the input image, not the mask
+    choice = (choice // 2) * 2
+    st.session_state.sample_idx = choice
+    st.session_state.prediction_valid = False
+
+
+def description() -> str:
+    """Return the description text associated with the current overlay."""
+    opt = st.session_state.overlay
+    if opt == 'IoU':
+        prefix = f'#### IoU score: {st.session_state.iou:.2f}'
+    else:
+        prefix = ''
+    return prefix + st.session_state.descriptions[opt]
 
 
 def main():
-    """Displays the solar panel segmentation app"""
-    app_model = make_default_app_model()
-    app_view = View()
-    controller = MainController(app_view, app_model)
-    controller.run()
+    """Defines the UI and connects it to the app logic."""
+    app_state.initialise_state()
+    # Title
+    st.markdown(
+        f"<h1 style='text-align: center'>{st.session_state.title}</h1>",
+        unsafe_allow_html=True
+    )
+    sample_img = load_sample_image()
+    st.image(sample_img, st.session_state.img_caption, use_column_width=True)
+    with st.container(border=True):
+        left, middle, right = st.columns(3)
+        left.button(
+            'Show another image!', on_click=choose_random_img_mask_pair
+        )
+        middle.selectbox(
+            'Overlay', st.session_state.overlay_opts, key='overlay'
+        )
+        right.slider(
+            'Confidence Threshold (%)',
+            min_value=10, max_value=90, step=10,
+            value=st.session_state.default_clf_threshold,
+            key='clf_threshold',
+            help="""The higher the confidence,
+                the more sure the model is there's a solar panel.
+                \n\nGreater than 50% means it thinks the highlighted 
+                area is more likely to be a solar panel than not."""
+        )
+    with st.container(border=True):
+        st.write(description())
 
 
 if __name__ == "__main__":
---
+++ app_state.py
@@ -0,0 +1,88 @@
+"""Functions to initialise the state of the app."""
+
+from pathlib import Path
+
+import streamlit as st
+import torch
+import torchvision.transforms.v2 as tv_transforms
+
+from models.fcn_factory import FcnFactory
+from preprocess.transforms import rgba_to_rgb
+
+
+def initialise_state():
+    """Initialises the state for the app. Edit values to alter app properties."""
+    initial_state = {
+        'title': 'Solar Panel Segmentation',
+        'test_ds_path': Path('data\\test_ds'),
+        'num_samples': 465,
+        'sample_idx': 0,
+        'img_path': None,
+        'mask_path': None,
+        'img_caption': '',
+        'overlay_alpha': 0.3,
+        'overlay_opts': ['None', 'True Mask', 'Prediction', 'IoU'],
+        'overlay': 'None',
+        'clf_path': 'saved_models/model_20240328_074804_4',
+        'default_clf_threshold': 50,
+        'prediction_valid': False,
+        'iou': 0.0,
+    }
+    for k, v in initial_state.items():
+        if k not in st.session_state:
+            setattr(st.session_state, k, v)
+    # 'descriptions' setup after because it refers to 'iou'
+    if 'descriptions' not in st.session_state:
+        st.session_state.descriptions = {
+            'None':
+                """The images are taken from a collection of images the
+                model is not trained on.
+                \n\nThe dataset which the model was trained on is:
+                \n\n*Jiang Hou, Yao Lingand Liu Yujun, ‘Multi-resolution
+                dataset for photovoltaic panel segmentation from satellite
+                and aerial imagery’. Zenodo, Aug. 09, 2021. doi:
+                10.5281/zenodo.5171712.*""",
+            'True Mask':
+                """The true mask is the area in which the solar panels
+                actually are.
+                \n\nHowever, you may notice some errors in these - any
+                errors like this in the training data will affect the
+                quality of the model.""",
+            'Prediction':
+                """This is the prediction made by the model.""",
+            'IoU':
+                """\n\n The *Intersection Over Union* (IoU) score is the
+                metric used to evaluate each prediction. It is simply the
+                value of the intersection divided by the union, where:
+                \n\n- The *intersection* is the area where the prediction
+                and the true mask both agree that there is a solar panel
+                (yellow).
+                \n\n- The *union* is the total area covered by either the
+                prediction or the mask (purple).
+                \n\nThe more purple there is, the more mistakes the model
+                has made.""",
+        }
+    factory = FcnFactory(n_classes=2)
+    if 'classifier' not in st.session_state:
+        st.session_state.classifier = load_clf_from(factory)
+    if 'in_transforms' not in st.session_state:
+        st.session_state.in_transforms = load_input_transforms_from(factory)
+
+
+def load_clf_from(factory: FcnFactory) -> torch.nn.Module:
+    """Load pretrained classifier from factory."""
+    loaded_clf = factory.make_fcn('resnet50')
+    loaded_clf.load_state_dict(torch.load(
+        st.session_state.clf_path,
+        map_location=torch.device('cpu')
+    ))
+    loaded_clf.eval()
+    return loaded_clf
+
+
+def load_input_transforms_from(factory: FcnFactory) -> tv_transforms.Transform:
+    """Load the input transforms for the pretrained classifier
+    from the factory."""
+    input_transforms = [tv_transforms.Lambda(rgba_to_rgb)]
+    input_transforms.append(factory.input_transforms)
+    return tv_transforms.Compose(input_transforms)
