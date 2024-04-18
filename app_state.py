d4c387eeabe542f17e78943e3626b98401e36ec0
ADPriceless <47830638+ADPriceless@users.noreply.github.com>
2024-04-18 16:50:07 +0100

    change classifier path from type str to pathlib.Path

--- app_state.py
+++ app_state.py
@@ -23,7 +23,7 @@ def initialise_state():
         'overlay_alpha': 0.3,
         'overlay_opts': ['None', 'True Mask', 'Prediction', 'IoU'],
         'overlay': 'None',
-        'clf_path': 'app_resources\\pretrained_models\\pretrained_solar_panel_clf.pt',
+        'clf_path': Path('app_resources\\pretrained_models\\pretrained_solar_panel_clf.pt'),
         'default_clf_threshold': 50,
         'prediction_valid': False,
         'iou': 0.0,
